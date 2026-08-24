5b2768ee2263b7c46ea56fa835c6d35f19f21f4a
Czekel <ignacypalinski995@gmail.com>
2026-08-24 18:03:14 +0200

    Update SupplyLoot.java

--- supplymod/src/main/java/com/supplymod/SupplyLoot.java
+++ supplymod/src/main/java/com/supplymod/SupplyLoot.java
@@ -14,38 +14,48 @@ import java.util.Random;
  * in the same crate, exactly as requested.
  *
  * Percentages as given:
- *  - Blok diamentu        30%
+ *  - Blok diamentu       30%
  *  - Totem Niesmiertelnosci 5%
  *  - Zlote jablko zaklete (Notch Apple) 10%
  *  - Zlote jablko          39%
  *  - Brodawki nether       25%
  *  - Czesc diamentowej zbroi (losowa) 40%
  *  - Zelazo + Zloto        70%
- *  - Zaczarowana ksiazka   30% (patrz uwaga nizej)
  *  - Serce (item)          10%
- *  - Ksiega Ulaskawienia    1%
- *  - Netherite scrap        5%
+ *  - Ksiega Ulaskawienia   1%
+ *  - Netherite scrap       5%
  *  - Diamenty (luzem)      30% (bylo 20%, +10pp na prosbe)
  *  - Miedz                 80%
  *
- *  Wypelniacze dodane przeze mnie, zeby zrzut nie byl pusty gdy nic
- *  wiekszego nie wypadnie:
+ * Wypelniacze dodane przeze mnie, zeby zrzut nie byl pusty gdy nic
+ * wiekszego nie wypadnie:
  *  - Chleb                 60%
  *  - Strzaly                50%
  *  - Proch strzelniczy      35%
  *  - Sznurek                40%
  *  - Lapis lazuli           25%
  *  - Szmaragd               15%
- *  - Perla Endermana        12%
- *  - Siodlo                  8%
+ *  - Perla Endermana        15%
+ *  - Siodlo                 5%
  *
- *  UWAGA (MC 1.21.1): zaklecia sa teraz elementem dynamicznego rejestru
- *  danych (podobnie jak biomy) i nie da sie ich pobrac statycznie z klasy
- *  Registries jak we wczesniejszych wersjach - wymagaja dostepu do
- *  RegistryWrapper w czasie dzialania serwera. Zeby nie blokowac reszty
- *  moda, ten wpis daje na razie zwykla (nie-zaklieta) Zaczarowana Ksiazke.
- *  Realne losowe zaklecie mozna dodac pozniej przekazujac np. ServerWorld
- *  do rollLoot() i czytajac world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).
+ * Dodatkowe przedmioty dorzucone na prosbe:
+ *  - Elytra                 1%
+ *  - Shulker box            5%
+ *  - Trojzab                10%
+ *  - Kusza                  20%
+ *  - Luk                    35%
+ *  - Blok szmaragdu         10%
+ *  - Blok zelaza            25%
+ *  - Blok zlota             20%
+ *  - Obsydian (luzem)       30%
+ *
+ * UWAGA (MC 1.21.1): zaklecia sa teraz elementem dynamicznego rejestru
+ * danych (podobnie jak biomy) i nie da sie ich pobrac statycznie z klasy
+ * Registries jak we wczesniejszych wersjach - wymagaja dostepu do
+ * RegistryWrapper w czasie dzialania serwera. Zeby nie blokowac reszty
+ * moda, ten wpis daje na razie zwykla (nie-zaklieta) Zaczarowana Ksiazke.
+ * Realne losowe zaklecie mozna dodac pozniej przekazujac np. ServerWorld
+ * do rollLoot() i czytajac world.getRegistryManager().get(RegistryKeys.ENCHANTMENT).
  */
 public class SupplyLoot {
 
@@ -77,6 +87,18 @@ public class SupplyLoot {
         roll(random, 0.12, () -> new ItemStack(Items.ENDER_PEARL, 1 + random.nextInt(2)), loot);
         roll(random, 0.08, () -> new ItemStack(Items.SADDLE, 1), loot);
 
+        // Dodatkowe przedmioty.
+        roll(random, 0.03, () -> new ItemStack(Items.ELYTRA, 1), loot);
+        roll(random, 0.08, () -> randomShulkerBox(random), loot);
+        roll(random, 0.04, () -> new ItemStack(Items.TRIDENT, 1), loot);
+        roll(random, 0.20, () -> new ItemStack(Items.CROSSBOW, 1), loot);
+        roll(random, 0.35, () -> new ItemStack(Items.BOW, 1), loot);
+        roll(random, 0.10, () -> new ItemStack(Items.EMERALD_BLOCK, 1), loot);
+        roll(random, 0.25, () -> new ItemStack(Items.IRON_BLOCK, 1), loot);
+        roll(random, 0.20, () -> new ItemStack(Items.GOLD_BLOCK, 1), loot);
+        roll(random, 0.30, () -> new ItemStack(Items.OBSIDIAN, 2 + random.nextInt(5)), loot);
+        roll(random, 0.15, () -> new ItemStack(Items.FIREWORK_ROCKET, 3 + random.nextInt(6)), loot);
+
         return loot;
     }
 
@@ -94,5 +116,12 @@ public class SupplyLoot {
                 new ItemStack(Items.DIAMOND_BOOTS)
         };
         return pieces[random.nextInt(pieces.length)];
+    }
+
+    private static ItemStack randomShulkerBox(Random random) {
+        ItemStack[] boxes = new ItemStack[]{
+                new ItemStack(Items.SHULKER_BOX)
+        };
+        return boxes[random.nextInt(boxes.length)];
     }
 }
