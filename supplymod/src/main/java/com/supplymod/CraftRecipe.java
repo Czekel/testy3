050cbb47582eb841d5aaf14271f7cb1167af6c3e
Czekel <ignacypalinski995@gmail.com>
2026-08-26 13:57:29 +0200

    Add nameColor parameter to CraftRecipe constructor

--- supplymod/src/main/java/com/supplymod/CraftRecipe.java
+++ supplymod/src/main/java/com/supplymod/CraftRecipe.java
@@ -1,17 +1,20 @@
 package com.supplymod;
 
 import net.minecraft.item.Item;
+import net.minecraft.util.Formatting;
 
 import java.util.List;
 
 public class CraftRecipe {
     public final Item resultItem;
     public final String displayName;
+    public final Formatting nameColor;
     public final List<Ingredient> ingredients;
 
-    public CraftRecipe(Item resultItem, String displayName, List<Ingredient> ingredients) {
+    public CraftRecipe(Item resultItem, String displayName, Formatting nameColor, List<Ingredient> ingredients) {
         this.resultItem = resultItem;
         this.displayName = displayName;
+        this.nameColor = nameColor;
         this.ingredients = ingredients;
     }
 
